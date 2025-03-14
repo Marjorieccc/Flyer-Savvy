58a43ac10834430b841486ad33fc13c9c10e6fee
danwongwt <wtwong.dan@gmail.com>
2025-03-13 20:11:56 -0400

    Feat: Enhance flyer, price history and point history database queries

--- application/src/lib/db/queries/flyer.ts
+++ application/src/lib/db/queries/flyer.ts
@@ -9,7 +9,7 @@ export async function GetAllFlyers():Promise<Flyer[] | null>{
 }
 
 // Return all on sale flyers' records from database
-export async function GetAllValidFlyers():Promise<Flyer[] | null>{
+export async function GetAllValidFlyers():Promise<Flyer[]>{
     const today = new Date().toISOString().split('T')[0];
     const flyers = await db.query.flyer.findMany({
         where: (gte(schema.flyer.valid_to, sql`${today}`))
@@ -18,21 +18,19 @@ export async function GetAllValidFlyers():Promise<Flyer[] | null>{
     return flyers;
 }
 
-
 // Return all flyerID of on sale flyers' records from database
 export async function GetAllValidFlyerID():Promise<number[] | null>{
-    const today = new Date().toISOString().split('T')[0];
-    const flyers = await db.query.flyer.findMany({
-        where: (gte(schema.flyer.valid_to, sql`${today}`))
-      });
-      const flyerIds: number[] = flyers.map(flyer => flyer.flyer_id);
+    const validFlyers: Flyer[]= await GetAllValidFlyers();
+    if(!validFlyers || validFlyers.length < 1) return null;
+
+    const flyerIds: number[] = validFlyers.map(flyer => flyer.flyer_id);
     return flyerIds;
 }
 
 
-export async function GetFlyerByID(flyerID:Number):Promise<Flyer | null>{
+export async function GetFlyerByID(flyerID:number):Promise<Flyer | null>{
     const flyer = await db.query.flyer.findFirst({
-        where :(eq(schema.flyer,flyerID ))
+        where :(eq(schema.flyer.flyer_id,flyerID ))
     });
     return flyer? flyer : null;
 }
--- application/src/lib/db/queries/priceHistory.ts
+++ application/src/lib/db/queries/priceHistory.ts
@@ -16,3 +16,19 @@ export async function GetPriceHistoryByProduct(product_id: number):Promise<Price
     });
     return priceHistory? priceHistory : null;
 }
+
+export async function GetPriceHistoryByFlyer(flyer_id: number):Promise<PriceHistory[] | null>{
+    const priceHistory= await db.query.price_history.findMany({
+        where :(eq(schema.price_history.flyer_id,flyer_id ))
+    });
+    return priceHistory? priceHistory : null;
+}
+
+export async function GetProductByPriceHistory(price_history_id: number):Promise< number | null>{
+    const result = await db
+        .select({product_id: schema.price_history.product_id})
+        .from(schema.price_history)
+        .where(eq(schema.price_history.price_history_id,price_history_id));
+
+    return result.length > 0 ? result[0].product_id : null;
+}
