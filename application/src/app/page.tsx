64049a58cace8b050192cca8a8648baab32306ec
Marjorieccc <marjorie.cc.cheung@gmail.com>
2025-04-04 19:36:47 -0400

    style: update search bar style

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,5 +1,10 @@
+import SearchBar from "@/components/search/SearchBar";
 import styles from "./page.module.scss";
 
 export default function Home() {
-  return <div className={styles.contentWrapper}></div>;
+  return (
+    <div className={styles.contentWrapper}>
+      <SearchBar />
+    </div>
+  );
 }
