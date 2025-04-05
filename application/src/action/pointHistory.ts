016090ac8dcc57ccb32fa0b76d13d0df833ee741
danwongwt <wtwong.dan@gmail.com>
2025-04-05 19:39:19 -0400

    fix: revise transforms function to synchronous

--- application/src/action/pointHistory.ts
+++ application/src/action/pointHistory.ts
@@ -1,30 +1,31 @@
-import * as Server from '@/types/server/product';
-import * as Api from '@/types/client/api';
-import * as Query from '@/lib/db/queries/pointHistory'
-import { GetAllValidFlyerID } from '@/lib/db/queries/flyer';
-import * as Transform from '@/lib/utils/transform';
+import * as Server from "@/types/server/product";
+import * as Api from "@/types/client/api";
+import * as Query from "@/lib/db/queries/pointHistory";
+import { GetAllValidFlyerID } from "@/lib/db/queries/flyer";
+import { transformPointHistory } from "@/lib/utils/transform";
 
+export async function findCurrentPointHistory(): Promise<
+  Api.PointHistory[] | null
+> {
+  // Fetch the current flyers
+  const currentFlyerIDs: number[] | null = await GetAllValidFlyerID();
 
-export async function findCurrentPointHistory():Promise<Api.PointHistory[] | null>{
-    // Fetch the current flyers
-    const currentFlyerIDs : number[] | null = await GetAllValidFlyerID();
+  // Check if flyers are found
+  if (currentFlyerIDs) {
+    // Fetch point histories concurrently for all flyers
+    const pointHistoryPromise = await Promise.all(
+      currentFlyerIDs.map((flyerID) => Query.GetPointHistoryByFlyer(flyerID))
+    );
+    // Filter out null values and flatten the results
+    const currentPointHistories: Server.PointHistory[] = pointHistoryPromise
+      .filter((pointHistory) => pointHistory !== null)
+      .flat() as Server.PointHistory[];
 
-    // Check if flyers are found
-    if(currentFlyerIDs){
-         // Fetch point histories concurrently for all flyers
-        const pointHistoryPromise  = await Promise.all(
-            currentFlyerIDs.map((flyerID) => Query.GetPointHistoryByFlyer(flyerID))
-        );
-        // Filter out null values and flatten the results
-        const currentPointHistories : Server.PointHistory[] = pointHistoryPromise.
-        filter((pointHistory) =>pointHistory !== null)
-        .flat() as Server.PointHistory[];
-
-        // Transform the point histories if any are found
-        return currentPointHistories.length>0? await Promise.all(currentPointHistories.map(Transform.transformPointHistory)) : null;
-    }
-    // Return null if no point histories are found or flyers are missing
-    return null;
+    // Transform the point histories if any are found
+    return currentPointHistories.length > 0
+      ? currentPointHistories.map(transformPointHistory)
+      : null;
+  }
+  // Return null if no point histories are found or flyers are missing
+  return null;
 }
-
-
--- application/src/service/server/product.ts
+++ application/src/service/server/product.ts
@@ -1,9 +1,11 @@
-import * as Server from '@/types/server/product';
-import * as Api from '@/types/client/api';
-import * as Query from '@/lib/db/queries/product';
-import * as Transform from '@/lib/utils/transform';
+import * as Server from "@/types/server/product";
+import * as Api from "@/types/client/api";
+import * as Query from "@/lib/db/queries/product";
+import { transformProduct } from "@/lib/utils/transform";
 
-export async function findProduct(productid:number):Promise<Api.Product|null>{
-    const product:Server.Product | null = await Query.GetProductByID(productid);
-    return product? await Transform.transformProduct(product): null;
+export async function findProduct(
+  productid: number
+): Promise<Api.Product | null> {
+  const product: Server.Product | null = await Query.GetProductByID(productid);
+  return product ? transformProduct(product) : null;
 }
